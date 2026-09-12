9c252793f36ee3f0d60b4d7614c561f7598e2429
Krishanu Bera <berakrishanu473@gmail.com>
2026-09-12 20:22:14 +0530

    Implement post-hog

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <div className='flex'>
+        <h2>404 |&nbsp;</h2>
+        <p>Page not found</p>
+      </div>
+    </main>
+  )
+}
+
+export default NotFound
